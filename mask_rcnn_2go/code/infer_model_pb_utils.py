60d2e693851314d4c8a2c2cf6d065c8fd918a088
David Reiss <dreiss@fb.com>
2019-05-22 13:44:34 -0700

    Use fixed input shape for keypoints

--- mask_rcnn_2go/code/infer_model_pb_utils.py
+++ mask_rcnn_2go/code/infer_model_pb_utils.py
@@ -160,6 +160,9 @@ def run_single_kpts(
         pixel_stds=pixel_stds,
     )
 
+    if inputs["data"].shape != (1,3,320,320):
+        return None
+
     # Prepare inputs for AABB and Int8AABB operators
     im_info = inputs["im_info"]
     scale = im_info[0][2]
